ef9474f04d8e2766ba3c8d55789950b4bfc602e0
Kenny Peeters <Kenny Peeters@192.168.0.227>
2016-04-19 11:24:36 +0200

    TEST

---
+++ KennyP_Test/be.cubis.box/res/js/component.js
@@ -0,0 +1,23 @@
+define(["sap/designstudio/sdk/component", "css!../css/component.css"], function(Component, css) {
+	Component.subclass("be.cubis.box.Box", function() {
+
+		var that = this;
+
+		this.init = function() {
+			this.$().addClass("coloredBox");
+			this.$().click(function() {
+				that.fireEvent("onclick");
+			});
+		};
+
+		this.color = function(value) {
+			if (value === undefined) {
+				return this.$().css("background-color");
+			} else {
+				this.$().css("background-color", value);
+				return this;
+			}
+		};
+	});	
+});
+    
